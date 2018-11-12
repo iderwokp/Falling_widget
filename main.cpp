2149e1cea83ced39d8b272220bc8c93c3459e737
Paul <paul.raestad@gmail.com>
2018-11-12 19:29:40 +0100

    To planeter rundt en planet

--- main.cpp
+++ main.cpp
@@ -34,8 +34,9 @@ int main(int argc, char** argv) {
     bool nedovery{true};
     bool nedoverx{true};
     //Falling_widget(std::string fn, SDL_Renderer* rend, Point p={0, 0}, int w = 0, int h = 0, int bound, int wbound,double vx0 = 0, double vy0 = 0,int rot= 0)
-    Falling_widget fw("ball.bmp", renderer, Point{350.0, 350.0}, 30, 20, windows_height, windows_width, 0, 2.5, 0);
-    Falling_widget fw2("ball.bmp", renderer, Point{850.0, 350.0}, 30, 20, windows_height, windows_width, 0, -2.5, 0);
+    Falling_widget fw("ball.bmp", renderer, Point{850.0, 350.0}, 3, 2, windows_height, windows_width, 0, -10, 0);
+    Falling_widget fw2("ball.bmp", renderer, Point{650.0, 350.0}, 30, 20, windows_height, windows_width, 0, 0, 0);
+    Falling_widget fw3("ball.bmp", renderer, Point{550.0, 350.0}, 3, 2, windows_height, windows_width, 0, 12, 0);
 //    float aksy = 0.98f;
 //    float aksx = 0.98f;
 //    fw.set_aksellerasjon(aksx, aksy);
@@ -43,7 +44,8 @@ int main(int argc, char** argv) {
     const double midwinX = windows_width/2;
     const double midwinY = windows_height/2;
     int index{300};
-    float gravitasjon{9.81/100};
+    float gravitasjon{9.81/1};
+    float gravitasjon2{9.81/1500};
     while(index >=0 && !quit) {
 	
         EventHandler(event, quit, windows_width, windows_height);
@@ -52,34 +54,50 @@ int main(int argc, char** argv) {
         int xpos = fw.current_pos().X;
         int ypos2 = fw2.current_pos().Y;
         int xpos2 = fw2.current_pos().X;
-        
-        double vecX = midwinX - xpos;
-        double vecY = midwinY - ypos;
-        double vecX2 = midwinX - xpos2;
-        double vecY2 = midwinY - ypos2;
+        int ypos3 = fw3.current_pos().Y;
+        int xpos3 = fw3.current_pos().X;
+//        double vecX = midwinX - xpos;
+//        double vecY = midwinY - ypos;
+//        double vecX2 = midwinX - xpos2;
+//        double vecY2 = midwinY - ypos2;
+        double vecX = xpos2 - xpos;
+        double vecY = ypos2 - ypos;
+        double vecX2 = xpos - xpos2;
+        double vecY2 = ypos - ypos2;
+        double vecX3 = xpos2 - xpos3;
+        double vecY3 = ypos2 - ypos3;
         double length_vecXY = sqrt(vecX*vecX + vecY*vecY);
         double length_vecXY2 = sqrt(vecX2*vecX2 + vecY2*vecY2);
+        double length_vecXY3 = sqrt(vecX3*vecX3 + vecY3*vecY3);
         double justert_lengde = length_vecXY/100;
         double justert_lengde2 = length_vecXY2/100;
+        double justert_lengde3 = length_vecXY3/100;
         
         double ekspr = (vecX*vecX+vecY*0)/(length_vecXY*vecX);
         double ekspr2 = (vecX2*vecX2+vecY2*0)/(length_vecXY2*vecX2);
+        double ekspr3 = (vecX3*vecX3+vecY3*0)/(length_vecXY3*vecX3);
         double radangl = acos(ekspr);
         double radangl2 = acos(ekspr2);
-        if (ypos > midwinY) radangl *= -1;
-        if (ypos2 > midwinY) radangl2 *= -1;
+        double radangl3 = acos(ekspr3);
+        if (ypos > ypos2) radangl *= -1;
+        if (ypos2 > ypos) radangl2 *= -1;
+        if (ypos3 > ypos2) radangl3 *= -1;
         
         int angle = static_cast<int>(radangl*(180.0/3.1415926));
         int angle2 = static_cast<int>(radangl2*(180.0/3.1415926));
+        int angle3 = static_cast<int>(radangl3*(180.0/3.1415926));
         
         float grav_rr = grav_avstand(justert_lengde, gravitasjon);
-        float grav_rr2 = grav_avstand(justert_lengde2, gravitasjon);
+        float grav_rr2 = grav_avstand(justert_lengde2, gravitasjon2);
+        float grav_rr3 = grav_avstand(justert_lengde3, gravitasjon);
         //std::cout << "grav_rr = " << grav_rr << "\tjustert_lengde^2 = " << justert_lengde * justert_lengde<<"\n";
         fw.set_aksellerasjon(grav_rr, angle);
         fw2.set_aksellerasjon(grav_rr2, angle2);
+        fw3.set_aksellerasjon(grav_rr3, angle3);
         
         auto [endX, endY] = endKoord(xpos, ypos, radangl, length_vecXY);
         auto [endX2, endY2] = endKoord(xpos2, ypos2, radangl2, length_vecXY2);
+        auto [endX3, endY3] = endKoord(xpos3, ypos3, radangl3, length_vecXY3);
         
         
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
@@ -87,12 +105,13 @@ int main(int argc, char** argv) {
 	    
 	    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
         
-		SDL_RenderDrawLine(renderer, 0,midwinY, windows_width,midwinY);
-		SDL_RenderDrawLine(renderer, midwinX,0, midwinX, windows_height);
-		SDL_RenderDrawLine(renderer, xpos,ypos, endX, endY);
-		SDL_RenderDrawLine(renderer, xpos2,ypos2, endX2, endY2);
+		//SDL_RenderDrawLine(renderer, 0,midwinY, windows_width,midwinY);
+		//SDL_RenderDrawLine(renderer, midwinX,0, midwinX, windows_height);
+		//SDL_RenderDrawLine(renderer, xpos,ypos, endX, endY);
+		//SDL_RenderDrawLine(renderer, xpos2,ypos2, endX2, endY2);
 		fw.updateXY();
 		fw2.updateXY();
+		fw3.updateXY();
 		SDL_RenderPresent(renderer);
 		SDL_Delay(30); 
        
@@ -112,7 +131,7 @@ std::pair<double, double> endKoord(double startX, double startY, double rad_vink
 }
 
 float grav_avstand(double avstand, float g) {
-	if(avstand< 1.0f) avstand = 1.0f;
+	if(avstand< 5.0f) avstand = 5.0f;
 	return g/(avstand*avstand);
 	
 }
