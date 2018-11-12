48fe12ed5f7075a16cfc5597ccc40ab59b8b7a95
Paul Raestad <paulor@online.no>
2018-11-12 10:22:31 +0100

    Starte å lage gravitasjon mot punkt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <SDL.h>
 #include <iostream>
+#include <cmath>
 #include "sdl_wrap.h"
 #include "Falling_widget.h"
 
+std::pair<double, double> endKoord(double startX, double startY, double rad_vinkel, double lengde);
 
 void EventHandler(SDL_Event event, bool& quit, int ww, int wh) {
     SDL_PollEvent(&event);
@@ -31,49 +33,52 @@ int main(int argc, char** argv) {
     bool nedovery{true};
     bool nedoverx{true};
     //Falling_widget(std::string fn, SDL_Renderer* rend, Point p={0, 0}, int w = 0, int h = 0, int bound, int wbound,double vx0 = 0, double vy0 = 0,int rot= 0)
-    Falling_widget fw("ball.bmp", renderer, Point{10.0, 100.0}, 30, 20, windows_height, windows_width, 0, 0, 0);
-    float aksy = 0.98f;
-    float aksx = 0.98f;
-    fw.set_aksellerasjon(aksx, aksy);
+    Falling_widget fw("ball.bmp", renderer, Point{300.0, 300.0}, 30, 20, windows_height, windows_width, 0, 0, 0);
+//    float aksy = 0.98f;
+//    float aksx = 0.98f;
+//    fw.set_aksellerasjon(aksx, aksy);
+    
+    const double midwinX = windows_width/2;
+    const double midwinY = windows_height/2;
     int index{300};
     
     while(index >=0 && !quit) {
 	
         EventHandler(event, quit, windows_width, windows_height);
-        fw.updateXY();
+        //fw.updateXY();
         int ypos = fw.current_pos().Y;
         int xpos = fw.current_pos().X;
+        double vecX = midwinX - xpos;//static_cast<double>(x);
+        double vecY = midwinY - ypos;//static_cast<double>(y);
+        double length_vecXY = sqrt(vecX*vecX + vecY*vecY);
+        double ekspr = (vecX*vecX+vecY*0)/(length_vecXY*vecX);
+        double radangl = acos(ekspr);
+        int angle = static_cast<int>(radangl*(180.0/3.1415926));
+        fw.set_aksellerasjon(0.0981, angle);
+        auto [endX, endY] = endKoord(xpos, ypos, radangl, length_vecXY);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer); 
+	    
+	    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
         
-        SDL_RenderPresent(renderer);
-        //SDL_RenderClear(renderer);  
-		SDL_Delay(15); 
-        if(nedovery == true && ypos >= windows_height/2  ) {
-			//fw.set_aksellerasjon(aksx, -aksy);
-			aksy *=-1;
-			nedovery = false;
-			
-		}
-       if(nedovery == false && ypos <= windows_height/2  ) {
-			//fw.set_aksellerasjon(aksx, aksy);
-			aksy *=-1;
-			nedovery = true;
-			
-		}
-		
-		 if(nedoverx == true && xpos >= windows_width/2  ) {
-			//fw.set_aksellerasjon(aksx, -aksy);
-			aksx *= -1;
-			nedoverx = false;
-			
-		}
-       if(nedoverx == false && xpos <= windows_width/2  ) {
-			//fw.set_aksellerasjon(aksx, aksy);
-			aksx *= -1;
-			nedoverx = true;
-			
-		}
-		fw.set_aksellerasjon(aksx, aksy);
+		SDL_RenderDrawLine(renderer, 0,midwinY, windows_width,midwinY);
+		SDL_RenderDrawLine(renderer, midwinX,0, midwinX, windows_height);
+		SDL_RenderDrawLine(renderer, xpos,ypos, endX, endY);
+		fw.updateXY();
+		SDL_RenderPresent(renderer);
+		SDL_Delay(30); 
+       
+       
+       
     }
    
     SDL_Quit();
+}
+
+std::pair<double, double> endKoord(double startX, double startY, double rad_vinkel, double lengde) {
+	
+	double x = lengde*cos(rad_vinkel);
+	double y = lengde*sin(rad_vinkel);
+	return std::make_pair(x+startX, y+startY);
+	
 }
