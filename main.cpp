b5329aa739912d195bac2d9fd2b7b1742a0ed927
Paul Raestad <paulor@online.no>
2019-03-11 08:49:46 +0100

    Fra fedora

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -50,23 +50,23 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
     constexpr float speed_konstant{0.1};
     constexpr int fwidget_width{30};
     constexpr int fwidget_height{20};
-    constexpr int start_fart{0};
+    constexpr int start_fart{35};
     std::vector<Falling_widget> baller;
     //baller.emplace_back("ball.bmp", renderer, Point{750.0, 350.0}, fwidget_width, fwidget_height, windows_height, windows_width, 0, -2, 0);
 
 
 //    baller.emplace_back("ball.bmp", renderer, Point{500.0, 500.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, -start_fart*speed_konstant, 0);
-    baller.emplace_back("ball.bmp", renderer, Point{550.0, 300.0}, fwidget_width, fwidget_height, windows_height, windows_width, -start_fart*speed_konstant, 0*speed_konstant, 0);
-    baller.emplace_back("ball.bmp", renderer, Point{650.0, 400.0}, fwidget_width, fwidget_height, windows_height, windows_width, -start_fart*speed_konstant, -0*speed_konstant, 0);
-    baller.emplace_back("ball.bmp", renderer, Point{550.0, 500.0}, fwidget_width+10, fwidget_height+10, windows_height, windows_width, -start_fart*speed_konstant, 0*speed_konstant, 0);
-    baller.emplace_back("ball.bmp", renderer, Point{650.0, 550.0}, fwidget_width, fwidget_height, windows_height, windows_width, 0*speed_konstant, 0*speed_konstant, 0);
-    //baller.emplace_back("ball.bmp", renderer, Point{650.0, 500.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, -0*speed_konstant, 0);
-  //  baller.emplace_back("ball.bmp", renderer, Point{700.0, 500.0}, fwidget_width, fwidget_height, windows_height, windows_width, 0*speed_konstant, -0*speed_konstant, 0);
-//    baller.emplace_back("ball.bmp", renderer, Point{630.0, 555.0}, fwidget_width, fwidget_height, windows_height, windows_width, 0*speed_konstant, 0*speed_konstant, 0);
-//    baller.emplace_back("ball.bmp", renderer, Point{444.0, 333.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, 0*speed_konstant, 0);
-//    baller.emplace_back("ball.bmp", renderer, Point{800.0, 50.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, -0*speed_konstant, 0);
-//    baller.emplace_back("ball.bmp", renderer, Point{250.0, 100.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, 0*speed_konstant, 0);
-//    baller.emplace_back("ball.bmp", renderer, Point{300.0, 70.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, 0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{250.0, 200.0}, fwidget_width, fwidget_height, windows_height, windows_width, start_fart*speed_konstant, 0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{650.0, 400.0}, fwidget_width, fwidget_height, windows_height, windows_width, -start_fart*speed_konstant, -start_fart*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{550.0, 600.0}, fwidget_width, fwidget_height, windows_height, windows_width, start_fart*speed_konstant, 0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{150.0, 550.0}, fwidget_width, fwidget_height, windows_height, windows_width, start_fart*speed_konstant, start_fart*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{850.0, 200.0}, fwidget_width, fwidget_height, windows_height, windows_width, -start_fart*speed_konstant, -0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{700.0, 500.0}, fwidget_width, fwidget_height, windows_height, windows_width, 0*speed_konstant, -0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{630.0, 555.0}, fwidget_width, fwidget_height, windows_height, windows_width, 0*speed_konstant, 0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{444.0, 333.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, 0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{800.0, 50.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, -0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{250.0, 100.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, 0*speed_konstant, 0);
+    baller.emplace_back("ball.bmp", renderer, Point{300.0, 70.0}, fwidget_width, fwidget_height, windows_height, windows_width, -0*speed_konstant, 0*speed_konstant, 0);
 
 
 
@@ -74,7 +74,7 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
 
 
     int index{1};
-    constexpr float gravitasjon{9.81*speed_konstant*speed_konstant};
+    constexpr float gravitasjon{14*9.81*speed_konstant*speed_konstant};
     //float gravitasjon2{9.81/1500};
     while(index >=0 && !quit) {
 
@@ -95,20 +95,30 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
 
 	        double acc_vec_Y{0};
 	        double acc_vec_X{0};
-			std::cout << " --------------------------------------------------------------------------------------------------------- " << std::endl;
+			//std::cout << " --------------------------------------------------------------------------------- " << std::endl;
         	for(const auto& sjekke_mot_ball: baller) {
         		if(gjeldende_ball == sjekke_mot_ball) continue;
 
         		Vec2d<double> sjekke_mot_ball_vec{sjekke_mot_ball.current_pos().X, sjekke_mot_ball.current_pos().Y};
         		Vec2d<double> temp_vec = sjekke_mot_ball_vec - start_vec;
         		double r = temp_vec.length();
-        		temp_vec.set_magnitude(200.0/r*r);
-        		std::cout << "r = " << r << " temp_vec.length() = " << temp_vec.length() << "  200/r*r = " << 200.0/r*r << std::endl;
-        		
+        		if(r<10) r = 10;
+        		double temp_x = temp_vec.xVal();
+        		double temp_y = temp_vec.yVal();
+//        		if(abs(temp_x) < 1) temp_x = 0.0;
+//        		if(abs(temp_y) < 1) temp_y = 0.0;
+        		double temp_angle = angle_deg(temp_vec, x_axe);
+        		double temp_angle_sanitized = sanitize_angle(Point{gjeldende_ball.current_pos().X, gjeldende_ball.current_pos().Y}, Point{temp_x, temp_y}, temp_angle);
+        		temp_vec = {40000.0/(r*r), static_cast<int> (temp_angle_sanitized)};
+        		//NB husk å sette Falling_widget::drag_til noe sånt som f.eks 0.997
+        		//temp_vec = {40000.0/(r*r), temp_angle_sanitized, true};
+        		//temp_vec.set_magnitude(40000.0/(r*r));
+        		//std::cout << "r = " << r << " temp_vec.length() = " << temp_vec.length() << "  4000/(r*r) = " << 4000.0/(r*r) << std::endl;
+
         		acc_vec += (temp_vec);
         	}
 //TODO: Avstanden mellom to objekter bør bestemme magnituden på vektorene, og ikke bare på resultant vektoren.
-
+//TODO: Sette individuell gravitasjon
         	acc_vec_X = acc_vec.xVal();
         	acc_vec_Y = acc_vec.yVal();
 
@@ -156,8 +166,8 @@ std::pair<double, double> endKoord(double startX, double startY, double rad_vink
 
 float grav_avstand(double avstand, float g) {
 	if(avstand< 5.0f) avstand = 5.0f;
-	//return g/(avstand*avstand);
-	return g;
+	return g/(avstand*avstand);
+	//return g;
 }
 
 
